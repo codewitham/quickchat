0f82f8d57e571e18656882ffc5f64b68cb8e2ddc
SniperGod <103439202+AkshatMahadeva@users.noreply.github.com>
2023-08-03 16:51:15 +0530

    revoke

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,6 +8,13 @@ const Navbar = () => {
     <header className='sticky top-0 flex justify-between items-center px-5 md:px-10 py-4 w-full'>
       <h1 className='text-2xl font-bold'>Quick Chat</h1>
       <div className='flex gap-5 items-center'>
+        <button onClick={() => {
+          localStorage.removeItem("api");
+          window.location.reload(false)
+        }}
+          className=' bg-indigo-500 text-white rounded-md px-2 py-2 text-sm'>
+          Revoke Key
+        </button>
         <UserButton />
       </div>
     </header>

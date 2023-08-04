6df1569eff9d4ed25772edd25a53ffc6b1e51475
SniperGod <103439202+AkshatMahadeva@users.noreply.github.com>
2023-08-04 09:11:35 +0530

    hello world

--- src/components/DisplayChat.jsx
+++ src/components/DisplayChat.jsx
@@ -4,18 +4,18 @@ import Image from 'next/image'
 import bot from "../../public/chatgpt.webp"
 import { useUser } from '@clerk/nextjs'
 
-const DisplayChat = ({message, reply}) => {
-  const {user} = useUser()
+const DisplayChat = ({ message, reply }) => {
+  const { user } = useUser()
   return (
     <div className='flex flex-col w-full'>
-        <div className='flex gap-4 bg-[#f1f1f1] p-5'>
-            <Image className='rounded-full h-[35px] w-[35px] shadow-sm' src={bot} alt='bot'/>
-            <p>{message}</p>
-        </div>
-        <div className='flex gap-4 text-gray-700 py-10 px-5'>
-            <img className='rounded-full h-[35px] w-[35px] shadow-sm object-cover' src={user?.profileImageUrl} alt={user?.fullName} />
-            <p>{reply}</p>
-        </div>
+      <div className='flex gap-4 bg-[#f1f1f1] p-5'>
+        <img className='rounded-full h-[35px] w-[35px] shadow-sm object-cover' src={user?.profileImageUrl} alt={user?.fullName} />
+        <p>{message}</p>
+      </div>
+      <div className='flex gap-4 text-gray-700 py-10 px-5'>
+        <Image className='rounded-full h-[35px] w-[35px] shadow-sm' src={bot} alt='bot' />
+        <p>{reply}</p>
+      </div>
     </div>
   )
 }
